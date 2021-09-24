3e57c55ae8346cbe638878a5c558a9ea45dafc02
François Farge <francoisf@theodo.fr>
2021-09-24 17:34:14 +0200

    feat: add forum-schemas

---
+++ common/forum-schemas/schemas/requests/getThreadWithPosts.ts
@@ -0,0 +1,14 @@
+import { FromSchema } from 'json-schema-to-ts';
+import { postEntitySchema, threadEntitySchema } from 'schemas/entities';
+
+export const getThreadReponseSchema = {
+  type: 'object',
+  properties: {
+    thread: threadEntitySchema,
+    posts: { type: 'array', items: postEntitySchema },
+  },
+  required: ['thread', 'posts'],
+  additionalProperties: false,
+} as const;
+
+export type GetThreadReponse = FromSchema<typeof getThreadReponseSchema>;

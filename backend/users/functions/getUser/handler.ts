2b50c028a47dee317b4c5d2fabc55bd03efe2162
fargito <fr.farge96@gmail.com>
2021-10-12 17:05:02 +0200

    feat: add users-schemas

--- backend/users/functions/getUser/handler.ts
+++ backend/users/functions/getUser/handler.ts
@@ -1,15 +1,16 @@
 import { applyHttpMiddlewares } from '@sls-monorepo/serverless-helpers';
 import { FromSchema } from 'json-schema-to-ts';
+import { UserEntity } from '@sls-monorepo/users-schemas';
 import { inputSchema } from './schema';
 
 export const handler = async (
   event: FromSchema<typeof inputSchema>,
-): Promise<string> => {
+): Promise<UserEntity> => {
   const { userId } = event.pathParameters;
 
   await Promise.resolve({ userId });
 
-  return 'ok';
+  return { userId, userName: 'hello_world' };
 };
 
 export const main = applyHttpMiddlewares(handler, { inputSchema });

550dee90826e797c917291b15b2de1a39b09b7d6
fargito <fr.farge96@gmail.com>
2021-10-19 17:16:14 +0200

    feat: add deployedVersion command

--- packages/serverless-tag-git-commit-plugin/plugin/serverlessTagGitCommitPlugin.ts
+++ packages/serverless-tag-git-commit-plugin/plugin/serverlessTagGitCommitPlugin.ts
@@ -1,3 +1,4 @@
+import * as AWS from 'aws-sdk';
 import * as Serverless from 'serverless';
 import * as Plugin from 'serverless/classes/Plugin';
 import simpleGit from 'simple-git';
@@ -17,9 +18,15 @@ export class ServerlessTagGitCommitPlugin implements Plugin {
   constructor(serverless: Serverless, options: OptionsExtended) {
     this.options = options;
     this.serverless = serverless;
-    this.commands = {};
+    this.commands = {
+      deployedVersion: {
+        usage: 'Get the git commit hash of the deployed version of the stack',
+        lifecycleEvents: ['describeStack'],
+      },
+    };
     this.hooks = {
       'after:package:initialize': this.tagStackWithGitCommit.bind(this),
+      'deployedVersion:describeStack': this.getDeployedGitCommitHash.bind(this),
     };
   }
 
@@ -36,5 +43,23 @@ export class ServerlessTagGitCommitPlugin implements Plugin {
       ...this.serverless.service.provider.stackTags,
       [TAG_NAME]: hash,
     };
+  }
+
+  async getDeployedGitCommitHash(): Promise<void> {
+    const provider = this.serverless.getProvider('aws');
+
+    const stackName = provider.naming.getStackName();
+
+    const { Stacks } = (await provider.request(
+      'CloudFormation',
+      'describeStacks',
+      {
+        StackName: stackName,
+      },
+    )) as AWS.CloudFormation.DescribeStacksOutput;
+
+    if (Stacks !== undefined) {
+      console.log(Stacks[0].Tags?.find(({ Key }) => Key === TAG_NAME)?.Value);
+    }
   }
 }

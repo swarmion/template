f4ea86ffb661d99162a7ca51979fcf8fb238adf5
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2021-10-30 08:18:43 +0200

    chore(jest): use shared config in ts

--- commonConfiguration/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  coverageReporters: ['json', ['lcov', { projectRoot: './' }]],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-};

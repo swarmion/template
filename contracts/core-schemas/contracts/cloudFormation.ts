6a1672c5c858eaf8962050ce774a3e86ad6c0b28
fargito <fr.farge96@gmail.com>
2021-11-10 21:49:53 +0100

    fix(core): change httpApi name

--- contracts/core-schemas/contracts/cloudFormation.ts
+++ contracts/core-schemas/contracts/cloudFormation.ts
@@ -1,5 +1,5 @@
 import { CloudFormationContract } from '@sls-monorepo/serverless-contracts';
 
 export const httpApiResourceContract = new CloudFormationContract({
-  key: '${self:custom.projectName}-HttpApiId-${self:provider.stage}',
+  key: 'CoreHttpApi',
 });

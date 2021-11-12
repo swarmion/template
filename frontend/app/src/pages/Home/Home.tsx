8c6d63c8326e15a49a39491712356f42e6bab0f0
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2021-11-12 17:18:04 +0100

    feat(frontend): use getUserContract to request user

--- frontend/app/src/pages/Home/Home.tsx
+++ frontend/app/src/pages/Home/Home.tsx
@@ -1,26 +1,58 @@
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
+import { useAsync, useMountEffect } from '@react-hookz/web/esnext';
+import { nanoid } from '@reduxjs/toolkit';
 import { FormattedMessage } from 'react-intl';
 
-import { Title } from 'components';
+import { getUserContract } from '@sls-monorepo/users-schemas';
 
-const Home = (): JSX.Element => (
-  <Box
-    display="flex"
-    flexDirection="column"
-    justifyContent="center"
-    alignContent="center"
-    textAlign="center"
-    height="100vh"
-    maxWidth="100%"
-  >
-    <Title />
-    <Box marginTop={6}>
-      <Button variant="contained">
-        <FormattedMessage id="home.button" />
-      </Button>
+import { Title } from 'components';
+import client from 'services/networking/client';
+
+const Home = (): JSX.Element => {
+  const [{ result, error }, { execute }] = useAsync(() =>
+    getUserContract.axiosRequest(client, {
+      pathParameters: { userId: nanoid() },
+    }),
+  );
+
+  useMountEffect(execute);
+
+  if (error) {
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignContent="center"
+        textAlign="center"
+        height="100vh"
+        maxWidth="100%"
+      >
+        Error: {error.message}
+      </Box>
+    );
+  }
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignContent="center"
+      textAlign="center"
+      height="100vh"
+      maxWidth="100%"
+    >
+      <Title />
+      <Box marginTop={6}>
+        <Button variant="contained" onClick={execute}>
+          <FormattedMessage id="home.button" />
+        </Button>
+      </Box>
+      <Box marginTop={6}>{JSON.stringify(result?.data)}</Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default Home;
--- frontend/app/src/translations/fr-FR.ts
+++ frontend/app/src/translations/fr-FR.ts
@@ -2,6 +2,6 @@ export default {
   home: {
     title:
       'Bienvenue sur le frontend de serverless-monorepo-microservices-template',
-    button: 'Je suis un bouton',
+    button: 'Refaire le call API',
   },
 };

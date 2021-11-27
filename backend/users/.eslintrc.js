f30b6f196b4e648860f4d3a474445cfbe375bf40
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2021-11-27 18:59:17 +0100

    feat(contracts): use eslint-plugin

--- backend/users/.eslintrc.js
+++ backend/users/.eslintrc.js
@@ -1,6 +1,7 @@
 const generateImportOrderRule = require('../../commonConfiguration/generateImportOrderRule');
 
 module.exports = {
+  extends: ['plugin:@serverless-contracts/recommended'],
   rules: generateImportOrderRule(__dirname),
   parserOptions: {
     project: ['./tsconfig.json'],

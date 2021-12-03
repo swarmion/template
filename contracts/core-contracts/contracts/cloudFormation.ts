524484ab6dac9a3a230ae8d29018080fed613f9f
fargito <fr.farge96@gmail.com>
2021-12-03 17:49:25 +0100

    feat(contracts): add id to contracts

--- contracts/core-contracts/contracts/cloudFormation.ts
+++ contracts/core-contracts/contracts/cloudFormation.ts
@@ -1,5 +1,6 @@
 import { CloudFormationContract } from '@serverless-contracts/core';
 
 export const httpApiResourceContract = new CloudFormationContract({
+  id: 'core-httpApi',
   name: 'CoreHttpApi',
 });

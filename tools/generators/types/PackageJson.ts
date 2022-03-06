8196cdaf21292b87040486834e4922d602c1038c
Guillaume Duboc <guillaumed@theodo.fr>
2022-03-06 10:49:07 +0100

    chore: update types for json files

--- tools/generators/types/PackageJson.ts
+++ tools/generators/types/PackageJson.ts
@@ -10,42 +10,23 @@ export interface PackageJson {
   module: string;
   types: string;
   scripts: {
-    [key: string]: unknown;
+    [key: string]: string;
     'lint-fix': string;
     'lint-fix-all': string;
     'linter-base-config': string;
-    package: string;
-    'package-cjs': string;
-    'package-esm': string;
-    'package-types': string;
     test: string;
     'test-linter': string;
     'test-type': string;
     'test-unit': string;
-    transpile: string;
-    watch: string;
   };
   dependencies: {
-    [key: string]: unknown;
-    '@babel/runtime': string;
+    [key: string]: string;
   };
   devDependencies: {
-    [key: string]: unknown;
-    '@babel/cli': string;
-    '@babel/core': string;
-    '@babel/plugin-transform-runtime': string;
-    '@babel/preset-env': string;
-    '@babel/preset-typescript': string;
+    [key: string]: string;
     '@types/node': string;
-    '@zerollup/ts-transform-paths': string;
-    'babel-plugin-module-resolver': string;
-    concurrently: string;
-    eslint: string;
     jest: string;
-    'json-schema-to-ts': string;
-    prettier: string;
     'ts-node': string;
-    ttypescript: string;
     typescript: string;
   };
 }

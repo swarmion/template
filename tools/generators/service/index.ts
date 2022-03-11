a9a3ccd03527bf080967ce0b3b5ede2cf70eb4fa
Guillaume Duboc <guillaumed@theodo.fr>
2022-03-11 17:34:03 +0100

    feat: generate service

---
+++ tools/generators/service/index.ts
@@ -0,0 +1,33 @@
+/* eslint-disable max-lines */
+import { formatFiles, installPackagesTask, Tree } from '@nrwl/devkit';
+import { join } from 'path';
+
+import { normalizeOptions, packageGenerator } from '../helpers';
+import { symlinkVsCodeConfiguration } from '../helpers/symlink';
+import { Schema } from '../types';
+import {
+  packageJson,
+  packageProjectJson,
+  packageTsConfig,
+} from './typed-json-config';
+
+const SOURCE_FOLDER = './files';
+
+export default async (tree: Tree, schema: Schema): Promise<() => void> => {
+  const options = normalizeOptions(tree, schema);
+
+  packageGenerator({
+    tree,
+    options,
+    sourcePath: join(__dirname, SOURCE_FOLDER),
+    packageJson,
+    packageProjectJson,
+    packageTsConfig,
+  });
+  await formatFiles(tree);
+
+  return () => {
+    symlinkVsCodeConfiguration(tree, options);
+    installPackagesTask(tree, true);
+  };
+};
